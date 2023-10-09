813ba669475eb28418de9300dcd2b8e89e0aa35b
Akash Tiwari <akash.tiwari21aug@gmail.com>
2023-10-09 10:20:29 +0530

    Time: 8 ms (67.04%), Space: 4 MB (6.37%) - LeetHub

---
+++ 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.go
@@ -0,0 +1,62 @@
+import "fmt"
+
+func findl(nums *[]int, l int, r int, target int) int {
+    // base cases
+    if l > r || l < 0 || r >= len(*nums){
+        return -1
+    }
+    mid := (l + r)/2
+
+    // found
+    if (*nums)[mid] == target{
+        // mid is the answer
+        if mid == 0 || (*nums)[mid-1] != target {
+            return mid
+        } else {
+            // search left
+            return findl(nums, l, mid-1, target )
+        }
+    } else if (*nums)[mid] < target{
+        // seach right
+        return findl(nums, mid+1, r, target)
+    } else {
+        // search left
+        return findl(nums, l, mid-1, target)
+    }
+
+}
+
+func findr(nums *[]int, l int, r int, target int) int {
+    // base cases
+    if l > r || l < 0 || r >= len(*nums){
+        return -1
+    }
+    mid := (l + r)/2
+
+    fmt.Printf("%d %d %d\n",l,r,mid)
+    // found
+    if (*nums)[mid] == target{
+        // mid is the answer
+        if mid == len(*nums)-1 || (*nums)[mid+1] != target {
+            return mid
+        } else {
+            // search right
+            return findr(nums, mid+1, r, target )
+        }
+    } else if (*nums)[mid] < target{
+        // seach right
+        return findr(nums, mid+1, r, target)
+    } else {
+        // search left
+        return findr(nums, l, mid-1, target)
+    }
+
+}
+
+
+func searchRange(nums []int, target int) []int {
+    var ans []int
+    ans = append(ans, findl(&nums,0,len(nums)-1,target))
+    ans = append(ans, findr(&nums,0,len(nums)-1,target))
+    return ans
+}
