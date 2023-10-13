29f78625562951fe4355ca4f4f82fc9d974519cd
Akash Tiwari <akash.tiwari21aug@gmail.com>
2023-10-13 09:50:29 +0530

    Time: 3 ms (68.37%), Space: 2.9 MB (57%) - LeetHub

--- 0747-min-cost-climbing-stairs/0747-min-cost-climbing-stairs.go
+++ 0747-min-cost-climbing-stairs/0747-min-cost-climbing-stairs.go
@@ -1,22 +1,17 @@
 func minCostClimbingStairs(cost []int) int {
-    // minCost[i] = min(minCost[i-1]+cost[i-1],minCost[i-2] + cost[i-1])
     n := len(cost)
-    var minCost [10001]int
 
-    for cur := 0 ;cur < n+1 ; cur ++ {
-        if(cur==0 || cur == 1){
-            minCost[cur] = 0
-        } else {
-            cost1 := minCost[cur-1] + cost[cur-1]
-            cost2 := minCost[cur-2] + cost[cur-2]
-            if(cost1<cost2){
-                minCost[cur] =  cost1
-            } else{
-                minCost[cur] = cost2
-            }
-            
+    for cur := 2 ;cur < n ; cur ++ {
+        if(cost[cur-1]<cost[cur-2]){
+            cost[cur] +=  cost[cur-1]
+        } else{
+            cost[cur] += cost[cur-2]
         }
     }
 
-    return minCost[n]
+    if cost[n-2]<cost[n-1] {
+        return cost[n-2]
+    } else {
+        return cost[n-1]
+    }
 }
