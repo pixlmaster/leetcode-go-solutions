0ba2fc13d27848edd6434a0946bf385e8e18f88b
Akash Tiwari <akash.tiwari21aug@gmail.com>
2024-04-11 12:46:36 +0530

    Time: 3 ms (86.96%) | Memory: 3.6 MB (34.78%) - LeetSync

--- 987-reveal-cards-in-increasing-order/reveal-cards-in-increasing-order.go
+++ 987-reveal-cards-in-increasing-order/reveal-cards-in-increasing-order.go
@@ -1,24 +1,53 @@
+type queue struct {
+    q []int
+    front int
+    back int
+}
+
+func createQ() *queue {
+    return &queue{make([]int,0) , 0 ,0}
+}
+
+func (q *queue)push(elem int) {
+    q.q = append(q.q,elem)
+    q.back++
+}
+
+func (q *queue)pop() {
+    q.front++
+}
+
+func (q *queue)top() int{
+    if q.isEmpty() {
+        return -1
+    }
+    return q.q[q.front]
+}
+
+func (q *queue)isEmpty() bool{
+    return q.front == q.back
+}
+
+
 func deckRevealedIncreasing(deck []int) []int {
-	n := len(deck)
-	sort.Ints(deck)
+    n := len(deck)
+    q := createQ()
+    sort.Ints(deck)
+    for i:=0 ; i<n;i++{
+        q.push(i)
+    }
 
-	res := make([]int, n)
-	queue := make([]int, n)
+    res := make([]int,n)
 
-	for i := range queue {
-		queue[i] = i
-	}
-
-	for _, card := range deck {
-		idx := queue[0]
-		queue = queue[1:]
+    for _, card := range deck {
+		idx := q.top()
+		q.pop()
 		res[idx] = card
 
-		if len(queue) > 0 {
-			queue = append(queue, queue[0])
-			queue = queue[1:]
+		if !q.isEmpty() {
+			q.push(q.top())
+            q.pop()
 		}
 	}
-
-	return res
+    return res
 }
