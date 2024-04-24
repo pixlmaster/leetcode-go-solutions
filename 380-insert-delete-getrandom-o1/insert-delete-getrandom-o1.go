537078c15cb95ce3b3bb060592e79580f47686d9
Akash Tiwari <akash.tiwari21aug@gmail.com>
2024-04-24 13:46:38 +0530

    Time: 155 ms (58.12%) | Memory: 45.6 MB (84.35%) - LeetSync

--- 380-insert-delete-getrandom-o1/insert-delete-getrandom-o1.go
+++ 380-insert-delete-getrandom-o1/insert-delete-getrandom-o1.go
@@ -1,61 +1,66 @@
-import "fmt"
-
 type RandomizedSet struct {
-    hset map[int]int
-    keyset []int
+    // hmap stores the value fo the element and the correspodning index in the list
+    hmap map[int]int
+    imap map[int]int
+    random []int
 }
 
 
 func Constructor() RandomizedSet {
-    randomisedSet := RandomizedSet{
-        hset : make(map[int]int),
-        keyset: make([]int,0),
-    }
-    return randomisedSet
+    return RandomizedSet{make(map[int]int), make(map[int]int), make([]int,0)}
 }
 
 
 func (this *RandomizedSet) Insert(val int) bool {
-    _, exists := this.hset[val]
-    // does not exist
-    if !exists {
-        // insert 
-        this.hset[val] = len(this.keyset)
-        this.keyset = append(this.keyset, val)
-        return true
+    _, exists := this.hmap[val]
+    if exists {
+        return false
     }
-    // already exists
-    return false
-
+    this.hmap[val] = len(this.random)
+    this.imap[len(this.random)] = val
+    this.random = append(this.random, val)
+    return true
 }
 
+func (this *RandomizedSet) printarr() {
+    for i:=0 ; i< len(this.random) ; i++ {
+        fmt.Println(this.random[i])
+    }
+}
 
 func (this *RandomizedSet) Remove(val int) bool {
-    index , exists := this.hset[val]
-    // exists
+    index, exists := this.hmap[val]
+    // fmt.Println("deletion" , val, exists)
+    // this.printarr()
     if exists {
-        // swap val with the last element in th keyset
-        this.keyset[index] = this.keyset[len(this.keyset)-1]
-        // change the index of the swapped element
-        this.hset[this.keyset[index]] = index
-        // delete the element
-        this.keyset = this.keyset[:len(this.keyset)-1]
-        delete(this.hset,val)
+        delete(this.hmap,val)
+        // swapped last element with the current element and reduce size by 1
+        this.random[index] = this.random[len(this.random)-1]
+        this.random = this.random[:len(this.random)-1]
+        // fmt.Println("post deletion ", index)
+        // this.printarr()
+        if len(this.random) > 0 && index < len(this.random){
+            // swapped element is now at the index
+            swappedVal := this.random[index]
+            // update swappedElem values
+            this.imap[index] = swappedVal
+            this.hmap[swappedVal] = index
+        }
+        delete(this.imap, len(this.random))
         return true
     }
-    // element did not exist
     return false
 }
 
-
 func (this *RandomizedSet) GetRandom() int {
-	// Generate a random integer between 0 and len(hset) -1
-	randomInt := rand.Intn(len(this.keyset))
-
-	// Retrieve the corresponding value
-	return this.keyset[randomInt]
-
+    if len(this.random) == 1{
+        return this.random[0]
+    }
+    r  := rand.Intn(len(this.random))
+    // fmt.Println(r)
+    return this.random[r]
 }
+
 
 
 /**
