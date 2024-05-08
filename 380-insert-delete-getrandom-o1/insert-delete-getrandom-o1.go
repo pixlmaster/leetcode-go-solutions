d2709a9adc9338fceeee7dcd806797a56bcc1b6d
Akash Tiwari <akash.tiwari21aug@gmail.com>
2024-05-08 14:32:59 +0530

    Time: 144 ms (84.57%) | Memory: 47.4 MB (62.91%) - LeetSync

--- 380-insert-delete-getrandom-o1/insert-delete-getrandom-o1.go
+++ 380-insert-delete-getrandom-o1/insert-delete-getrandom-o1.go
@@ -1,13 +1,13 @@
 type RandomizedSet struct {
     // hmap stores the value fo the element and the correspodning index in the list
     hmap map[int]int
-    imap map[int]int
+    // imap map[int]int
     random []int
 }
 
 
 func Constructor() RandomizedSet {
-    return RandomizedSet{make(map[int]int), make(map[int]int), make([]int,0)}
+    return RandomizedSet{make(map[int]int), make([]int,0)}
 }
 
 
@@ -17,7 +17,7 @@ func (this *RandomizedSet) Insert(val int) bool {
         return false
     }
     this.hmap[val] = len(this.random)
-    this.imap[len(this.random)] = val
+    // this.imap[len(this.random)] = val
     this.random = append(this.random, val)
     return true
 }
@@ -43,10 +43,10 @@ func (this *RandomizedSet) Remove(val int) bool {
             // swapped element is now at the index
             swappedVal := this.random[index]
             // update swappedElem values
-            this.imap[index] = swappedVal
+            // this.imap[index] = swappedVal
             this.hmap[swappedVal] = index
         }
-        delete(this.imap, len(this.random))
+        // delete(this.imap, len(this.random))
         return true
     }
     return false
