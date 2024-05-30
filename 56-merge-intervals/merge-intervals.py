326d8fba4e549dd33c0e3a2cc08b9162651541e6
Akash Tiwari <akash.tiwari21aug@gmail.com>
2024-05-30 20:57:20 +0530

    Time: 129 ms (27.84%) | Memory: 20.6 MB (52.19%) - LeetSync

--- 56-merge-intervals/merge-intervals.py
+++ 56-merge-intervals/merge-intervals.py
@@ -1,15 +1,15 @@
 class Solution:
-    def comp(self, a: List[int], b: List[int])-> int :
-        if a[0] < b[0]:
-            return -1
-        elif a[0] > b[0]:
-            return 1
-        return 0
+    # def comp(self, a: List[int], b: List[int])-> int :
+    #     if a[0] < b[0]:
+    #         return -1
+    #     elif a[0] > b[0]:
+    #         return 1
+    #     return 0
             
 
 
     def merge(self, intervals: List[List[int]]) -> List[List[int]]:
-        intervals = sorted(intervals, key=functools.cmp_to_key(self.comp))
+        intervals = sorted(intervals)
         n = len(intervals)
         ans = []
         i=0
