29d7db8e3eec519849c38c1a3303e598b2b636be
Akash Tiwari <akash.tiwari21aug@gmail.com>
2024-05-30 15:47:20 +0530

    Time: 108 ms (68.77%) | Memory: 17.2 MB (96.46%) - LeetSync

--- 76-minimum-window-substring/minimum-window-substring.py
+++ 76-minimum-window-substring/minimum-window-substring.py
@@ -19,15 +19,9 @@ class Solution:
         for i in range(n):
             if s[i] not in freq :
                 continue
-            # if included == target :
-            #     while s[start]  :
-            #         if s[start] not in freq:
-            #             start+=1
-            #         else:
-            #             included -=1
-            #             currfreq[s[start]]-=1
-            #             start+=1
-
+            # if we have already reached the target, we must have done the shortening in the prev loop
+            # remove the first character which is guranteed to be significant
+            # this opens up one more space to extend our candidate further
             if included == target :
                 currfreq[s[start]]-=1
                 included -=1
@@ -40,25 +34,24 @@ class Solution:
             else :
                 currfreq[s[i]] = 1
 
-
+            # increment the frequency of the significant character, if it is not already up to the mark
             if currfreq[s[i]] <= freq[s[i]] :
                 included += 1
 
-
-            # print(s[start:i+1])
-            # print(currfreq,included)
-            # reached a point where the substring has all of the characters or more
+            # if the significant characters reacht the expected mark, try removing stuff from the start
             while included == target :
                 if s[start] not in freq:
+                    # remove non significant characters
                     start+=1
                 elif  currfreq[s[start]] > freq[s[start]]:
+                    # freely remove characters are more than expected
                     currfreq[s[start]] -=1
                     start+=1
                 else:
+                    # if you enocunter a significant charact4er, do not remove it , curr string is a canditate
                     if i-start<minLen:
                         minLen = i - start
                         ansStart = start
-                        # print(s[start:i+1], s[ansStart:ansStart+minLen], ansStart,minLen)
                     break
         if ansStart == -1 :
             return ""
