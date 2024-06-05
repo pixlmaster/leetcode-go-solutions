b6dba0b20d0b75f4ddbffea6e543af73f268165b
Akash Tiwari <akash.tiwari21aug@gmail.com>
2024-06-05 21:59:33 +0530

    Time: 120 ms (56.09%) | Memory: 17.7 MB (96.39%) - LeetSync

--- 130-surrounded-regions/surrounded-regions.py
+++ 130-surrounded-regions/surrounded-regions.py
@@ -2,69 +2,42 @@ class Solution:
     o = "O"
     x = "X"
     a = "a"
-    v = "v"
 
     def solve(self, board: List[List[str]]) -> None:
         """
         Do not return anything, modify board in-place instead.
         """
-        # iterate over grid
-        #   if elem is 0
-        #       visit the region
-        #       if any region reaches the edge
-        #           do not modify anything
-        #       else modify to a third character
-        #       second pass modify third character to x
-
 
         row = len(board)
         col = len(board[0])
 
         for r in range(row):
-            for c in range(col):
-                if board[r][c] == self.o:
-                    shouldflip = not self.visit(board,r,c)
-
-
-                    if shouldflip:
-                        self.mark(board,r,c,self.a)
-                    else:
-                        self.mark(board,r,c,self.o)
-
+            self.visit(board,r,0)
+            self.visit(board,r,col-1)
+        for c in range(col):
+            self.visit(board,0,c)
+            self.visit(board,row-1,c)
 
         for r in range(row):
             for c in range(col):
                 if board[r][c] == self.a:
+                    board[r][c] = self.o
+                else:
                     board[r][c] = self.x
     
-    def visit(self,board,r,c) -> bool:
+    def visit(self,board,r,c) -> None:
         row = len(board)
         col = len(board[0])
 
-        if r<0 or r>=row or c<0 or c>=col:
-            return True
-        if board[r][c] != self.o :
-            return False
+        if r<0 or r>=row or c<0 or c>=col or board[r][c] != self.o:
+            return
 
-        board[r][c] = self.v
+        board[r][c] = self.a
 
-        return self.visit(board,r-1,c) or self.visit(board,r+1,c) or self.visit(board,r,c-1) or self.visit(board,r,c+1)
+        self.visit(board,r-1,c)
+        self.visit(board,r+1,c)
+        self.visit(board,r,c-1)
+        self.visit(board,r,c+1)
         
 
-    def mark(self,board,r,c, char):
-        row = len(board)
-        col = len(board[0])
-
-        if r<0 or r>=row or c<0 or c>=col:
-            return 
-
-        if board[r][c] != self.v :
-            return 
-
-        board[r][c] = char
-
-        self.mark(board,r-1,c,char)
-        self.mark(board,r+1,c,char)
-        self.mark(board,r,c-1,char)
-        self.mark(board,r,c+1,char)
         
