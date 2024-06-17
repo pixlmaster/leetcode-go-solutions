d877d26a0787c32f662583f7b146ec6cc391ec6f
Akash Tiwari <akash.tiwari21aug@gmail.com>
2024-06-17 11:01:10 +0530

    Time: 125 ms (27.58%) | Memory: 16.4 MB (96.81%) - LeetSync

---
+++ 633-sum-of-square-numbers/sum-of-square-numbers.py
@@ -0,0 +1,13 @@
+
+class Solution:
+    def judgeSquareSum(self, c: int) -> bool:
+
+        for num in range(0,c+1):
+            toSearch = c - num*num
+            if toSearch < 0:
+                break
+            if math.sqrt(toSearch)%1 ==0:
+                return True
+        
+        
+        return False
