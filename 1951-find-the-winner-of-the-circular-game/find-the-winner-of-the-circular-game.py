ddc30b94fe85255d62ec43cdca6491d8b152f68b
Akash Tiwari <akash.tiwari21aug@gmail.com>
2024-07-08 20:33:24 +0530

    Time: 32 ms (87.46%) | Memory: 16.8 MB (8.19%) - LeetSync

--- 1951-find-the-winner-of-the-circular-game/find-the-winner-of-the-circular-game.py
+++ 1951-find-the-winner-of-the-circular-game/find-the-winner-of-the-circular-game.py
@@ -5,7 +5,7 @@ class Solution:
             orig.append(i)
         start = 0
         while len(orig) > 1:
-            print("removing ", orig[(start + k%n - 1)%n])
+            # print("removing ", orig[(start + k%n - 1)%n])
             del orig[(start + k % n -1)%n]
             start = (start +k % n -1)%n
             # print("New start", start, orig[start])
