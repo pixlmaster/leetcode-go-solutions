8b042547a70bf6572eaebfb429cd9313c56b77f7
Akash Tiwari <akash.tiwari21aug@gmail.com>
2024-07-08 15:35:37 +0530

    Time: 40 ms (54.75%) | Memory: 16.7 MB (18.56%) - LeetSync

---
+++ 1951-find-the-winner-of-the-circular-game/find-the-winner-of-the-circular-game.py
@@ -0,0 +1,13 @@
+class Solution:
+    def findTheWinner(self, n: int, k: int) -> int:
+        orig = []
+        for i in range(1, n + 1):
+            orig.append(i)
+        start = 0
+        while len(orig) > 1:
+            print("removing ", orig[(start + k%n - 1)%n])
+            del orig[(start + k % n -1)%n]
+            start = (start +k % n -1)%n
+            # print("New start", start, orig[start])
+            n -= 1
+        return orig[0]
