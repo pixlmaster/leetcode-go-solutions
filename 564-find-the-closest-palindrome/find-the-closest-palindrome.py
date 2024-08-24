15daa5a3759608886d8823bbf48b21d17093262c
Akash Tiwari <akash.tiwari21aug@gmail.com>
2024-08-25 00:42:33 +0530

    Time: 33 ms (75.57%) | Memory: 16.7 MB (28.21%) - LeetSync

--- 564-find-the-closest-palindrome/find-the-closest-palindrome.py
+++ 564-find-the-closest-palindrome/find-the-closest-palindrome.py
@@ -6,14 +6,14 @@ class Solution:
         lenNum = len(numStr)
         for i in range(lenNum + 1):
             edge_cases.add(pow(10, i))
-            edge_cases.add(pow(10,i)+1)
+            edge_cases.add(pow(10, i) + 1)
 
         num = int(numStr)
         if lenNum == 1:
             return str(num - 1)
         elif num in edge_cases:
             n = len(numStr)
-            return str(pow(10,n-1)-1)
+            return str(pow(10, n - 1) - 1)
 
         midPoint = (lenNum - 1) // 2
         half1Str = numStr[:midPoint + 1]
@@ -24,25 +24,25 @@ class Solution:
             # simply copy the first half
             pos3 = half1Str + half1Str[::-1]
             pos3Num = int(pos3)
-            if half2Str != half1Str[::-1] and abs(pos3Num-num) < diff:
+            if half2Str != half1Str[::-1] and abs(pos3Num - num) < diff:
                 ans = pos3
                 # print(pos3)
-                diff = abs(pos3Num-num)
+                diff = abs(pos3Num - num)
             half1Num = int(half1Str)
-            firstHalfPlus1Str = str(half1Num+1)
-            firstHalfMinus1Str = str(half1Num-1)
+            firstHalfPlus1Str = str(half1Num + 1)
+            firstHalfMinus1Str = str(half1Num - 1)
             pos2 = firstHalfPlus1Str + firstHalfPlus1Str[::-1]
             pos2Num = int(pos2)
-            if abs(num-pos2Num) < diff:
+            if abs(num - pos2Num) < diff:
                 # print(pos2)
                 ans = pos2
-                diff = abs(num-pos2Num)
+                diff = abs(num - pos2Num)
             pos1 = firstHalfMinus1Str + firstHalfMinus1Str[::-1]
             pos1Num = int(pos1)
-            if abs(num-pos1Num) <= diff:
+            if abs(num - pos1Num) <= diff:
                 # print(pos1)
                 ans = pos1
-                diff = abs(num-pos1Num)
+                diff = abs(num - pos1Num)
         else:
             # calculate the 2 possibilities
             half1StrWithoutMid = half1Str[:-1]
@@ -51,33 +51,30 @@ class Solution:
             pos3 = half1StrWithoutMid + str(half1Str[-1]) + half1StrWithoutMid[::-1]
             pos3Num = int(pos3)
 
-            if pos3Num != num and abs(num-pos3Num) < diff:
+            if pos3Num != num and abs(num - pos3Num) < diff:
                 ans = pos3
-                diff = abs(num-pos3Num)
+                diff = abs(num - pos3Num)
             if midPos1 >= 0:
                 pos1 = half1StrWithoutMid + str(midPos1) + half1StrWithoutMid[::-1]
                 pos1Num = int(pos1)
-                if abs(num-pos1Num) <= diff:
+                if abs(num - pos1Num) <= diff:
                     ans = pos1
-                    diff = abs(num-pos1Num)
+                    diff = abs(num - pos1Num)
 
             if midPos2 < 10:
                 pos2 = half1StrWithoutMid + str(midPos2) + half1StrWithoutMid[::-1]
                 pos2Num = int(pos2)
-                if abs(num-pos2Num) < diff:
+                if abs(num - pos2Num) < diff:
                     ans = pos2
-                    diff = abs(num-pos2Num)
+                    diff = abs(num - pos2Num)
 
+        edgeCaseUpper = pow(10, lenNum) + 1
+        edgeCaseLower = pow(10, lenNum - 1) - 1
 
-        edgeCaseUpper = pow(10,lenNum)+1
-        edgeCaseLower = pow(10,lenNum-1)-1
-
-        if abs(edgeCaseUpper-num) < diff:
+        if abs(edgeCaseUpper - num) < diff:
             ans = str(edgeCaseUpper)
-            diff = abs(edgeCaseUpper-num)
-        if abs(edgeCaseLower-num) <= diff:
+            diff = abs(edgeCaseUpper - num)
+        if abs(edgeCaseLower - num) <= diff:
             ans = str(edgeCaseLower)
 
         return ans
-
-
